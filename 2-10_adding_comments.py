489230c183257785afd36b43f1e46388a4be23f1
mike <mike@endymion.fios-router.home>
2020-06-19 18:03:25 -0400

    forgot how2 write

---
+++ 2-10_adding_comments.py
@@ -0,0 +1,12 @@
+#This is a copy of exercise 2-6. This script stores a famous person's name as a variable, 
+#stores a quote from them as a variable within a sentence in which their name is used, and then prints the message.
+famous_person = "William Butler Yeats"
+message = f"A quote from {famous_person} went,\n'I'm looking for the face I had\nBefore the world was made.'"
+print(message)
+
+#This is a copy of exercise 2-8. This script calculates four simple arithmetic operations using the four basic 
+#operators in which they all produce the output of 8.
+print(1+7)
+print(432-424)
+print(2*4)
+print(int(64/8))
