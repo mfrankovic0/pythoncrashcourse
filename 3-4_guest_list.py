3b6d57263f4d4472d3a3793504dbb146c57c646f
mike <mike@endymion.fios-router.home>
2020-06-19 19:09:27 -0400

    v funny

---
+++ 3-4_guest_list.py
@@ -0,0 +1,5 @@
+guest_list = ['Dwayne "The Rock" Johnson', 'Doctor Phil', 'Albert Camus', 'Mikhail Gorbachev',]
+print(f"Greetings {guest_list[0]}, you are being formally invited to a dinner hosted by myself. RSVP at the return address promptly.")
+print(f"Greetings {guest_list[1]}, you are being formally invited to a dinner hosted by myself. RSVP at the return address promptly.")
+print(f"Greetings {guest_list[2]}, you are being formally invited to a dinner hosted by myself. RSVP at the return address promptly.")
+print(f"Greetings {guest_list[3]}, you are being formally invited to a dinner hosted by myself. RSVP at the return address promptly.")
