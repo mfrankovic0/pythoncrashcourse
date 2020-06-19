2655340cc7eb3ff6e8e673f76e86b743225bf07e
mike <mike@endymion.fios-router.home>
2020-06-19 18:21:59 -0400

    lulz

---
+++ 3-2_greetings.py
@@ -0,0 +1,5 @@
+friends = ['Xavier', 'Jose', 'Harry', 'Doug',]
+print(f"{friends[0]}, you are quite the dumbass I'll have you know.")
+print(f"{friends[1]}, you are quite the dumbass I'll have you know.")
+print(f"{friends[2]}, you are quite the dumbass I'll have you know.")
+print(f"{friends[3]}, you are quite the dumbass I'll have you know.")
