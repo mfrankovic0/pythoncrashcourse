c88d6a0b4716ba24b8b64f9199de54ed15fd4ac0
mike <mike@endymion.fios-router.home>
2020-06-19 18:27:39 -0400

    whoa

---
+++ 3-3_your_own_list.py
@@ -0,0 +1,6 @@
+car_wishlist = ['Koeningsegg Agera', '1969 Chevelle SS', 'McLaren F1', 'Toyota Corolla KE70', 'Porsche 911 993',]
+print(f'My dream car is a {car_wishlist[0]}.')
+print(f'My dream car is a {car_wishlist[1]}.')
+print(f'My dream car is a {car_wishlist[2]}.')
+print(f'My dream car is a {car_wishlist[3]}.')
+print(f'My dream car is a {car_wishlist[4]}.')
