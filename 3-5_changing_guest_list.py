d5a136b8026a68c048164737979e357800ea6acb
mike <mike@endymion.fios-router.home>
2020-06-19 19:18:11 -0400

    gettin better

---
+++ 3-5_changing_guest_list.py
@@ -0,0 +1,8 @@
+guest_list = ['Dwayne "The Rock" Johnson', 'Doctor Phil', 'Albert Camus', 'Mikhail Gorbachev',]
+dead = guest_list.pop(2)
+print(f"{dead} is unable to attend as he is dead.")
+guest_list.insert(2, 'Psy')
+print(f"Greetings {guest_list[0]}, you are being formally invited to a dinner hosted by myself. RSVP at the return address promptly.")
+print(f"Greetings {guest_list[1]}, you are being formally invited to a dinner hosted by myself. RSVP at the return address promptly.")
+print(f"Greetings {guest_list[2]}, you are being formally invited to a dinner hosted by myself. RSVP at the return address promptly.")
+print(f"Greetings {guest_list[3]}, you are being formally invited to a dinner hosted by myself. RSVP at the return address promptly.")
