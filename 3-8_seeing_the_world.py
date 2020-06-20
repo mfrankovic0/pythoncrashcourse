e8b5c44a0071edf691335b949cb50ac13b4b5aa8
mike <mike@endymion.fios-router.home>
2020-06-20 16:41:08 -0400

    lol florida

---
+++ 3-8_seeing_the_world.py
@@ -0,0 +1,22 @@
+world = ['Japan', 'Tibet', 'Florida', 'Algeria', 'Madagascar',]
+print(world)
+
+print(sorted(world))
+
+print(world)
+
+print(sorted(world, reverse=True))
+
+print(world)
+
+world.reverse()
+print(world)
+
+world.reverse()
+print(world)
+
+world.sort()
+print(world)
+
+world.sort(reverse=True)
+print(world)
