febc4324a54ec6563c3444db348330d9d39d6a50
mike <mike@endymion.fios-router.home>
2020-06-20 18:10:44 -0400

    again

---
+++ 3-9_dinner_guests.py
@@ -0,0 +1,3 @@
+guest_list = ['Larry David', 'Dwayne "The Rock" Johnson', 'Doctor Phil', 'Britney Spears', 'Psy', 'Mikhail Gorbachev', 'Cardi B']
+
+print(f"There will be {len(guest_list)} guests at the dinner party.")
