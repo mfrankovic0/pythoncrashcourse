d93b315fd08706abab7895f13a061795f25a506d
mike <mike@endymion.fios-router.home>
2020-06-20 19:01:06 -0400

    NJ

---
+++ 3-10_every_function.py
@@ -0,0 +1,48 @@
+njtowns = ['North Bergen', 'New Rochelle', 'Secaucus', 'Paramus', 'Edgewater', 'Franklin Lakes', 'New Brunswick',]
+
+print(njtowns)
+
+print(njtowns[3])
+
+print(njtowns[2].title())
+
+print(njtowns[-1])
+
+msg = f"I was born in {njtowns[2]}."
+print(msg)
+
+njtowns[5] = 'Ramsey'
+print(njtowns)
+
+njtowns.append('New Brunswick')
+print(njtowns)
+
+njtowns.insert(3, 'Teterboro')
+print(njtowns)
+
+del njtowns[7]
+print(njtowns)
+
+popped_town = njtowns.pop(7)
+print(f"I don't go to {popped_town} often.")
+
+too_small = 'Teterboro'
+njtowns.remove(too_small)
+print(f"{too_small} is too small of a town.")
+
+njtowns.sort()
+print(njtowns)
+
+njtowns.sort(reverse=True)
+print(njtowns)
+
+print(sorted(njtowns))
+print(njtowns)
+
+njtowns.sort()
+print(njtowns)
+
+njtowns.reverse()
+print(njtowns)
+
+print(len(njtowns))
