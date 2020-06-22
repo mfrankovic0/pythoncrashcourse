6c6e9af45a267eb8f3d3dfc12cbe9453164071a4
mike <mike@endymion.fios-router.home>
2020-06-22 19:01:40 -0400

    hungry

---
+++ 4-1_pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ['pepperoni', 'sausage', 'bacon', 'peppers', 'basil']
+
+for pizza in pizzas:
+    print(f"The best pizza has {pizza} on it!")
+
+print("But any pizza is better than none!")
