0cba0f1d0c315b031659fdf5c130c671afeb69b0
mike <mfrankovic34@gmail.com>
2020-06-22 20:05:37 -0400

    retry

--- 4-1_pizzas.py
+++ 4-1_pizzas.py
@@ -3,4 +3,4 @@ pizzas = ['pepperoni', 'sausage', 'bacon', 'peppers', 'basil']
 for pizza in pizzas:
     print(f"The best pizza has {pizza} on it!")
 
-print("But any pizza is better than none!")
+print("But any pizza is better than none!") 
