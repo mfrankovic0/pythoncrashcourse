29e4eef59c844417d53bf66494c309e9ad0a30d5
mike <mfrankovic34@gmail.com>
2020-07-03 17:19:42 -0400

    review THIS

---
+++ Chapter 4/4-15_code_review.py
@@ -0,0 +1,15 @@
+# Only copying one(Ex. 4-13) as the rest were PEP8 compliant
+
+buffet = ('chicken wings', 'french fries', 'pizza', 'steamed veggies', 'salad')
+
+for food in buffet:
+    print(food)
+
+# buffet.remove('pizza')
+# Line 6 returns error if uncommented as items from a tuple cannot be removed
+
+new_buffet = ('chicken wings', 'sweet potato',
+              'burger', 'steamed veggies', 'salad')
+
+for food in new_buffet:
+    print(food)
