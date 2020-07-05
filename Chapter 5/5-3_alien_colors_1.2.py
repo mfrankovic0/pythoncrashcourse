7f211770c5456d66c5b43c1088e95363b3ced3ae
Michael <mike@soren.fios-router.home>
2020-07-05 17:50:53 -0400

    road trippp

---
+++ Chapter 5/5-3_alien_colors_1.2.py
@@ -0,0 +1,5 @@
+alien_color = 'green'
+
+if alien_color == 'red':
+    print('You have earned 5 points!')
+    
---
+++ Chapter 5/5-3_alien_colors_1.py
@@ -0,0 +1,5 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('You have earned 5 points!')
+    
