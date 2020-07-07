6ae79a44d7f501d0122cab8e1fd636211b830743
mike <mfrankovic34@gmail.com>
2020-07-07 18:10:59 -0400

    fruity

---
+++ Chapter 5/5-7_favorite_fruit.py
@@ -0,0 +1,16 @@
+favorite_fruit = ['apple', 'pear', 'pineapple',]
+
+if 'orange' in favorite_fruit:
+    print('You really like oranges!')
+
+if 'apple' in favorite_fruit:
+    print('You really like apples!')
+
+if 'kiwi' in favorite_fruit:
+    print('You really like kiwis!')
+
+if 'pineapple' in favorite_fruit:
+    print('You really like pineapples!')
+
+if 'pear' in favorite_fruit:
+    print('You really like pears!')
