3214c9d99c106358b4d280fa45e8defba0e28865
mike <mfrankovic34@gmail.com>
2020-07-09 18:09:11 -0400

    confusing one

--- Chapter 5/5-10_checking_usernames.py
+++ Chapter 5/5-10_checking_usernames.py
@@ -1,8 +1,11 @@
 current_users = ['Jethro', 'Jebediah', 'Tabitha', 'Yolanda', 'Lancaster']
 new_users = ['Jebediah', 'Dakota', 'Dale', 'Ellie', 'Garth']
 
+
+current_users_lower = [user.lower() for user in current_users]
+
 for new_user in new_users:
-    if new_user in current_users:
+    if new_user.lower() in current_users_lower:
         print('This username is not available. Please select another one.')
     else:
         print('This username is available.')
