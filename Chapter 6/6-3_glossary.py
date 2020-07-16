01ca23357c59886ec84b59a6c5fa2560ebed28e6
mike <mfrankovic34@gmail.com>
2020-07-16 18:28:13 -0400

    fancified

--- Chapter 6/6-3_glossary.py
+++ Chapter 6/6-3_glossary.py
@@ -1,9 +1,9 @@
 glossary = {'indentation': 'the spaces at the beginning of a code line',
-    'list': 'a collection which is ordered and changeable, allows duplicate members',
-    'tuple': ' a collection which is ordered and unchangeable',
-    'variable': 'containers for storing data values',
-    'integer': 'a whole number, positive or negative, without decimals, of unlimited length'
-    }
+            'list': 'a collection which is ordered and changeable, allows duplicate members',
+            'tuple': ' a collection which is ordered and unchangeable',
+            'variable': 'containers for storing data values',
+            'integer': 'a whole number, positive or negative, without decimals, of unlimited length'
+            }
 
 definition = glossary['indentation']
 print(f"Indentation: {definition}")
