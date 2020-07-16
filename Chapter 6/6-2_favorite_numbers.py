869836be6b1f85fdbcd5ab4835c4fac1dfc0f1fc
mike <mfrankovic34@gmail.com>
2020-07-16 18:03:08 -0400

    I say YEET HAWWW

---
+++ Chapter 6/6-2_favorite_numbers.py
@@ -0,0 +1,21 @@
+people = {'Jenny': '3',
+    'Jimbob': '8',
+    'Marie': '1',
+    'Talbot': '14',
+    'Darleen': '37'
+    }
+
+fav_number = people['Jenny']
+print(f"Jenny's favorite number is {fav_number}.")
+
+fav_number = people['Jimbob']
+print(f"Jimbob's favorite number is {fav_number}.")
+
+fav_number = people['Marie']
+print(f"Maries favorite number is {fav_number}.")
+
+fav_number = people['Talbot']
+print(f"Talbot's favorite number is {fav_number}.")
+
+fav_number = people['Darleen']
+print(f"Darleen's favorite number is {fav_number}.")
