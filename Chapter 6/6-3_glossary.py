62328092dcee15b041945b08c175b37b164ddd1e
mike <mfrankovic34@gmail.com>
2020-07-16 18:27:39 -0400

    just got YEETED

---
+++ Chapter 6/6-3_glossary.py
@@ -0,0 +1,21 @@
+glossary = {'indentation': 'the spaces at the beginning of a code line',
+    'list': 'a collection which is ordered and changeable, allows duplicate members',
+    'tuple': ' a collection which is ordered and unchangeable',
+    'variable': 'containers for storing data values',
+    'integer': 'a whole number, positive or negative, without decimals, of unlimited length'
+    }
+
+definition = glossary['indentation']
+print(f"Indentation: {definition}")
+
+definition = glossary['list']
+print(f"List: {definition}")
+
+definition = glossary['tuple']
+print(f"Tuple: {definition}")
+
+definition = glossary['variable']
+print(f"Variable: {definition}")
+
+definition = glossary['integer']
+print(f"Integer: {definition}")
