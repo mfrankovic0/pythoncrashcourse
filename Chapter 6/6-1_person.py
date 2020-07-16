4d4ad33b37bd90c94cbe27bad5aeda864d8e7468
mike <mfrankovic34@gmail.com>
2020-07-16 17:48:42 -0400

    yeet haw

---
+++ Chapter 6/6-1_person.py
@@ -0,0 +1,10 @@
+person = {'first_name': 'Jethro',
+    'last_name': 'Johnson-Teller',
+    'age': '32',
+    'city': 'Chattanooga'
+    }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
