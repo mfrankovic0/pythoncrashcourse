e4feef2f6836289c2437d953f9a40b065e1d7506
mike <mfrankovic34@gmail.com>
2020-08-04 20:06:29 -0400

    yo daddyyy

---
+++ Chapter 8/8-11_archived_messges.py
@@ -0,0 +1,16 @@
+def send_messages(messages, sent_messages):
+    """Send a series of messsages to another list.."""
+    while messages:
+        sending = messages.pop()
+        print(f"Sending: {sending}")
+        sent_messages.append(sending)
+
+def print_messages():
+    print(sent_messages)
+    print(messages)
+
+messages = ['Hey there!', 'How are ya?', "What's up?", ]
+sent_messages = []
+
+send_messages(messages[:], sent_messages)
+print_messages()
---
+++ Chapter 8/8-9_messages.py
@@ -0,0 +1,7 @@
+def show_messages(messages):
+    """Print a series of messsages."""
+    for message in messages:
+        print(message)
+
+messages_list = ['Hey there!', 'How are ya?', "What's up?", ]
+show_messages(messages_list)
