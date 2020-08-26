14f04ff23250df06419541ee71fc93de1a148e21
mike <mfrankovic34@gmail.com>
2020-08-26 19:27:09 -0400

    getting real meta now

---
+++ Chapter 9/9-6_ice_cream_stand.py
@@ -0,0 +1,38 @@
+class Ice_cream_stand:
+    """An ice cream stand model."""
+
+    def __init__(self, name, kind):
+        """Initialize name and type."""
+        self.restaurant_name = name
+        self.cuisine_kind = kind
+        self.number = 0
+        self.flavors = []
+
+    def describe_restaurant(self):
+        """Describes restaurant name and type."""
+        print(f"{self.restaurant_name} is a restaurant that serves {self.cuisine_kind}.")
+    
+    def open_restaurant(self):
+        """Indicates if restaurant is open."""
+        print(f"{self.restaurant_name} is open.")
+
+    def number_served(self):
+        """Display number served."""
+        print(f'Serving {self.number} currently.')
+
+    def set_number_served(self, number):
+        """Sets number served."""
+        self.number = number
+
+    def increment_number_served(self, number):
+        """Sets number incrementlyby adding only."""
+        self.number += number
+
+    def display_flavors(self):
+        """Displays the flavors."""
+        print(f"Flavors are: {self.flavors}.")
+
+Gelato_de_Giovanni = Ice_cream_stand('Gelato de Giovanni', 'gelato')
+Gelato_de_Giovanni.flavors = ['vanilla', 'chocolate', 'mint', 'strawberry']
+
+Gelato_de_Giovanni.display_flavors()
---
+++ Chapter 9/9-8_privileges.py
@@ -0,0 +1,53 @@
+class User:
+    """Creates a user and their attributes."""
+
+    def __init__(self, first_name, last_name):
+        """Store first and last name."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.login_attempts = 0
+
+    def describe_user(self):
+        """Print attributes."""
+        print(f"First Name = {self.first_name}")
+        print(f"Last Name = {self.last_name}")
+
+    def greet_user(self):
+        """Greet the user using their name."""
+        print(f"Hello, {self.first_name} {self.last_name}!")
+
+    def increment_login_attempts(self):
+        """Add 1 to login attempts."""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Resets login attempts to 0."""
+        self.login_attempts = 0
+
+    def display_login_attempts(self):
+        """Self-explanatory."""
+        print(f"{self.first_name} {self.last_name} has made {self.login_attempts} login attempts.")
+
+
+class Admin(User):
+    """Create a user with superuser privileges."""
+
+    def __init__(self, first_name, last_name):
+        """Add user attributes and superuser privileges."""
+        super().__init__(first_name, last_name)
+        self.privileges = Privileges()
+
+class Privileges:
+    """Stores privileges and shows them."""
+
+    def __init__(self):
+        """Stores privileges."""
+        self.privileges = ['add post', 'del post', 'edit post', 'ban user', 'reinstate user']
+
+    def show_privileges(self):
+        """Show privileges."""
+        print(f"Privileges:\n {self.privileges}")
+
+Jimbob = Admin('Jimbob', 'Orton')
+
+Jimbob.privileges.show_privileges()
